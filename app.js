0696ab38d38f939db9874430144aca92b8c249ca
Order <mchontz10@gmail.com>
2019-02-24 21:22:01 -0500

    everything so far

---
+++ app.js
@@ -0,0 +1,395 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+console.log('Server started');
+console.log('Enabling express...');
+app.use('/',express.static(__dirname + '/client'));
+var httpServer = http.createServer(app);
+httpServer.listen(1498);
+console.log("Express started");
+var io = require('socket.io')(httpServer);
+
+var players = {};
+var sockets = {};
+var rings = {};
+
+var globalTimer = -175, lag = 0, ops = 0;
+var wheelRadius = 20, springiness = 1;
+var speedMult = .1;
+
+
+var Player = function(i){
+	var self = {
+		color:randRainbow(),
+		name:("Guest"+Math.random()*10000).substring(0,8),
+		trail:false,
+		pingTimer:500,
+		
+		timer:0,
+		finished:false,
+		
+		lx:0,
+		ly:0,
+		la:0,
+		lvx:0,
+		lvy:0,
+		lva:0,
+		
+		w1x:-2*wheelRadius,
+		w1y:0,
+		w1vx:0,
+		w1vy:0,
+		
+		w2x:2*wheelRadius,
+		w2y:0,
+		w2vx:0,
+		w2vy:0,
+		
+		id:i,
+		
+		w:false,
+		s:false,
+		a:false,
+		d:false,
+		space:false,
+	}
+	self.tick = function(){
+		if(self.pingTimer--<0){
+			var text = self.name + " disconnected!";
+			sendAll("chat", {msg:text});
+			delete players[self.id];
+			return;
+		}
+		if(globalTimer < 0 || self.finished)
+			return;
+		if(self.lx > 16384){
+			sendAll('chat', {msg:self.name+" crossed the finish line!"});
+			self.finished = true;
+		}
+		self.timer = globalTimer;
+		self.move();
+		if(!self.space)
+			self.collide();
+		if(self.ly > self.lx+2048)
+			self.respawn();
+	}
+	self.move = function(){
+		self.lvx += ((self.lx+self.w1x+self.w2x)/3.-self.lx)*.01;
+		self.lvy += ((self.ly+self.w1y+self.w2y)/3.-self.ly)*.01;
+		self.lx += ((self.lx+self.w1x+self.w2x)/3.-self.lx)*.15;
+		self.ly += ((self.ly+self.w1y+self.w2y)/3.-self.ly)*.15;
+		
+		var par1 = ((self.w1y+(self.ly-wheelRadius*2*Math.sin(self.la)))/2.-self.w1y)
+		var par2 = ((self.w1x+(self.lx-wheelRadius*2*Math.cos(self.la)))/2.-self.w1x)
+		var par3 = ((self.w2y+(self.ly+wheelRadius*2*Math.sin(self.la)))/2.-self.w2y)
+		var par4 = ((self.w2x+(self.lx+wheelRadius*2*Math.cos(self.la)))/2.-self.w2x)
+		
+		
+		self.w1vy += par1*.01;
+		self.w1vx += par2*.01;
+		self.w2vy += par3*.01;
+		self.w2vx += par4*.01;
+		self.w1y += par1*.7;
+		self.w1x += par2*.7;
+		self.w2y += par3*.7;
+		self.w2x += par4*.7;
+		
+		self.lvy+=.1;//gravity
+		self.w1vy+=.1;
+		self.w2vy+=.1;
+		
+		self.lx += self.lvx;
+		self.ly += self.lvy;
+		self.w1x += self.w1vx;
+		self.w1y += self.w1vy;
+		self.w2x += self.w2vx;
+		self.w2y += self.w2vy;
+		
+		var prior = square((self.w1y+(self.ly-wheelRadius*2*Math.sin(self.la)))/2.-self.w1y);
+		prior += square((self.w1x+(self.lx-wheelRadius*2*Math.cos(self.la)))/2.-self.w1x);
+		prior += square((self.w2y+(self.ly+wheelRadius*2*Math.sin(self.la)))/2.-self.w2y);
+		prior += square((self.w2x+(self.lx+wheelRadius*2*Math.cos(self.la)))/2.-self.w2x);
+		var post = square((self.w1y+(self.ly-wheelRadius*2*Math.sin(self.la+.01)))/2.-self.w1y);
+		post += square((self.w1x+(self.lx-wheelRadius*2*Math.cos(self.la+.01)))/2.-self.w1x);
+		post += square((self.w2y+(self.ly+wheelRadius*2*Math.sin(self.la+.01)))/2.-self.w2y);
+		post += square((self.w2x+(self.lx+wheelRadius*2*Math.cos(self.la+.01)))/2.-self.w2x);
+		self.lva = (prior-post)*.007;
+		
+		if(self.a)
+			self.lva -= .07;
+		if(self.d)
+			self.lva += .07;
+		self.la += self.lva;
+		self.lva *= .5;
+		
+		self.lvy *= .999;//air resistance
+		self.lvx *= .999;
+		self.w1vx *= .99;
+		self.w2vx *= .99;
+		self.w1vy *= .99;
+		self.w2vy *= .99;
+	}
+	self.collide = function(){
+		for(var i in rings){
+			var ring = rings[i];
+			var dist = Math.sqrt(hypot2(self.w1x,ring.x,self.w1y,ring.y));
+			var squish = Math.abs(dist - ring.r), squishFac = 2*Math.sqrt(wheelRadius-squish+1)-2;
+			if(squish<wheelRadius+1){
+				var flip = dist>ring.r?1:-1;
+				var speedMult = (self.s?-1:0)+(self.w?1:0);
+				var mult = springiness*flip/dist
+				self.w1vx -= speedMult*mult*(squishFac+2)*(self.w1y-ring.y);
+				self.w1vy += speedMult*mult*(squishFac+2)*(self.w1x-ring.x)/dist;
+				self.w1vx = self.w1vx*.8+mult*squishFac*(self.w1x-ring.x);
+				self.w1vy = self.w1vy*.8+mult*squishFac*(self.w1y-ring.y);
+			}
+			
+			dist = Math.sqrt(hypot2(self.w2x,ring.x,self.w2y,ring.y));
+			squish = Math.abs(dist - ring.r), squishFac = 2*Math.sqrt(wheelRadius-squish+1)-2;
+			if(squish<wheelRadius+1){
+				var flip = dist>ring.r?1:-1;
+				var speedMult = (self.s?-1:0)+(self.w?1:0);
+				var mult = springiness*flip/dist
+				self.w2vx -= mult*speedMult*(squishFac+2)*(self.w2y-ring.y);
+				self.w2vy += mult*speedMult*(squishFac+2)*(self.w2x-ring.x);
+				self.w2vx = self.w2vx*.8+mult*squishFac*(self.w2x-ring.x);
+				self.w2vy = self.w2vy*.8+mult*squishFac*(self.w2y-ring.y);
+			}
+		}
+	}
+	self.changeName = function(newName){
+		if(newName.length > 16 || pass.length < 1){
+			send(self.id, "chat", {msg:"Name must be 1-16 characters."});
+			return;
+		}
+		self.name = newName;
+		self.save();
+		send(self.id, "chat", {msg:"Name changed successfully."});
+	}
+	self.sendMap = function(){
+		send(self.id, "chat", {msg:"New map loaded."});
+		send(self.id, "newMap", {rings:rings});
+	}
+	self.respawn = function(){
+		self.trail = false;
+		self.finished = false;
+		self.lx = self.ly = self.w1y = self.w2y = 0;
+		self.w1x = -2*wheelRadius;
+		self.w2x = 2*wheelRadius;
+		self.lvx = self.lvy = self.w1vy = self.w2vy = self.w1vx = self.w2vx = 0;
+		self.la = self.lva = 0;
+		self.score *= .9;
+		self.timer = 0;
+	}
+	return self;
+}
+var Ring = function(i, x, y, r){
+	var self = {
+		color:randRainbow(),
+		id:i,
+		x:x,
+		y:y,
+		r:r,
+	}
+	return self;
+}
+
+
+
+function send(id, msg, data){
+	var s = sockets[id];
+	if(typeof s !== "undefined")
+		s.emit(msg, data);
+}
+
+
+
+io.sockets.on('connection', function(socket){
+	socket.id = Math.random();
+	sockets[socket.id]=socket;
+	var ip = socket.request.connection.remoteAddress;
+	
+	socket.on('requestBody',function(data){
+		var player = Player(socket.id);
+		players[socket.id]=player;
+		player.sendMap();
+	});
+	socket.on('pingmsg',function(data){
+		var player = players[socket.id];
+		if(typeof player === "undefined")
+			return;
+		socket.emit('reping', {time:data.time});
+		player.pingTimer = 250;
+	});
+	socket.on('disconnect',function(data){
+		var player = players[socket.id];
+		if(typeof player === "undefined")
+			return;
+		var text = player.name + " left the game!";
+		sendAll("chat", {msg:text});
+	});
+	socket.on('key',function(data){
+		var player = players[socket.id];
+		if(typeof player === "undefined")
+			return;
+		if(typeof data.inputId === 'undefined' || typeof data.state === 'undefined')
+			return;
+		if(data.inputId==='w')
+			player.w = data.state;
+		if(data.inputId==='s')
+			player.s = data.state;
+		if(data.inputId==='a')
+			player.a = data.state;
+		if(data.inputId==='d')
+			player.d = data.state;
+		if(data.inputId===' ')
+			player.space = data.state;
+	});
+	socket.on('chat',function(data){
+		var player = players[socket.id];
+		if(typeof player === "undefined")
+			return;
+		if(typeof data.msg !== "string")
+			return;
+		data.msg = data.msg.trim();
+		if(player.name !== "GUEST" && data.msg.startsWith("/")){
+			if(data.msg.startsWith("/name "))
+				player.changeName(data.msg.substring(6));
+			else
+				send(player.id, "chat", {msg:"Unknown Command."});
+			return;
+		}
+		if(typeof data.msg !== 'string' || data.msg.length == 0 || data.msg.length > 128)
+			return;
+		data.msg = (" "+data.msg+" ").replace(/fuck/ig, '****').replace(/fuk/ig, '****').replace(/vagina/ig, '******').replace(/fvck/ig, '****').replace(/penis/ig, '*****').replace(/slut/ig, '****').replace(/ tit /ig, ' *** ').replace(/ tits /ig, ' **** ').replace(/whore/ig, '****').replace(/shit/ig, '****').replace(/cunt/ig, '****').replace(/bitch/ig, '*****').replace(/faggot/ig, '******').replace(/ fag /ig, ' *** ').replace(/nigger/ig, '******').replace(/nigga/ig, '******').replace(/dick/ig, '****').replace(/ ass /ig, ' *** ').replace(/pussy/ig, '*****').replace(/ cock /ig, ' **** ').trim();
+		var spaces = "";
+		for(var i = player.name.length; i < 16; i++)
+			spaces += " ";
+			
+		const finalMsg = (player.name + ": " + data.msg);
+		if(player.globalChat == 0)
+			sendAll('chat', {msg:finalMsg});
+	});
+});
+function findBisector(a1, a2){
+	a1 = a1 * 180 / Math.PI;
+	a2 = a2 * 180 / Math.PI;
+	a1 = mod(a1, 360);
+	a2 = mod(a2, 360);
+	var small = Math.min(a1, a2);
+	var big = Math.max(a1, a2);
+	var angle = (big - small) / 2 + small;
+	if(big - small > 180)
+		angle += 180;
+	return angle * Math.PI / 180;
+}
+function atan(y, x){
+	var a = Math.min(abs(x), abs(y)) / Math.max(abs(x), abs(y));
+	var s = a * a;
+	var r = ((-0.0464964749 * s + 0.15931422) * s - 0.327622764) * s * a + a;
+	if (abs(y) > abs(x))
+		r = 1.57079637 - r;
+	if (x < 0)
+		r = 3.14159274 - r;
+	if (y < 0)
+		r = -r;
+	return r;
+}
+function square(x){
+	return x * x;
+}
+function abs(x){
+	return x > 0?x:-x;
+}
+function mod(n, m) {
+    var remain = n % m;
+    return Math.floor(remain >= 0 ? remain : remain + m);
+}
+function r128(){
+	return Math.floor(Math.random() * 128);
+}
+function hypot2(x1, x2, y1, y2){
+	return square(x1-x2)+square(y1-y2);
+}
+function randRainbow(){
+	var t = r128();
+	var str = ((Math.floor(Math.cos(t) * 128 + 128) << 16) + (Math.floor(Math.cos(t+Math.PI*2/3) * 128 + 128) << 8) + Math.floor(Math.cos(t+Math.PI * 4 / 3) * 128 + 128)).toString(16)
+	while (str.length < 6)
+		str = "0"+str;
+	return "#"+str;
+}
+
+
+
+function sendAll(out, data){
+	for(var i in sockets)
+		sockets[i].emit(out, data);
+}
+
+
+
+newRace();
+setTimeout(update,3000);
+function newRace(){
+
+	globalTimer = -150;
+	
+	var besti = 0;
+	for(var i in players){
+		if(besti == 0 && players[i].finished)
+			besti = i;
+		if(players[i].finished && players[i].timer < players[besti].timer)
+			besti = i;
+	}
+	
+	for(var i in players){
+		var player = players[i];
+		player.respawn();
+	}
+	
+	if(besti != 0){
+		sendAll('chat', {msg:players[besti].name+" won the round!"});
+		players[besti].trail = true;
+	}
+	
+	rings = {};
+	rings[0] = new Ring(0, 0, 0, 1024);
+	rings[0].color = "cyan"
+	outer:while(Object.keys(rings).length<800){
+		var id = Math.random();
+		var x = Math.random() * 16384, y = Math.random() * 16384, r = Math.pow(2, 6 + Math.random() * 4);
+		if(y > x)
+			continue outer;
+		for(var i in rings)
+			if(Math.hypot(x - rings[i].x, y - rings[i].y) < r + rings[i].r)
+				continue outer;
+		rings[id] = new Ring(id, x, y, r);
+	}
+	
+	for(var i in players)
+		players[i].sendMap();
+	
+}
+
+
+
+function update(){
+	ops++;
+	if(globalTimer++ > 6000)
+		newRace();
+	if(ops < 2)
+		setTimeout(update, 20);
+	var d = new Date();
+	var lagTimer = d.getTime();
+	var pack = {};
+	for(var i in players){
+		var player = players[i];
+		player.tick();
+		send(i, 'you', {lx:player.lx,ly:player.ly});
+		pack[player.id] = ({finished:player.finished,timer:player.timer,score:player.score,trail:player.trail,w1x:player.w1x,w1y:player.w1y,w2x:player.w2x,w2y:player.w2y,lx:player.lx,ly:player.ly,la:player.la,color:player.color,name:player.name,space:player.space});
+	}
+	sendAll('posUp', {players:pack, globalTimer:globalTimer});
+	ops--;
+}
+
---
+++ client/client.js
@@ -0,0 +1,401 @@
+if (location.protocol != 'http:')
+	location.href = 'http:' + window.location.href.substring(window.location.protocol.length);
+//require('https').globalAgent.options.rejectUnauthorized = false;
+
+var socket = io('128.113.192.44:1498');
+
+var canvas = document.getElementById('ctx');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext("2d");
+
+
+
+
+
+
+var shift = false;
+var keyboard = {};
+var typing = false;
+var chatLength = 30, chatScroll = 0, globalChat = 0, preChatArr = {}, chati = 0;
+var messages = {};
+for (var i = 0; i < chatLength; i++)
+	messages[i] = "";
+
+
+var ops = 0;
+var didW = false;
+var empty = 0;
+var multiplayer = true;
+
+var trails = {};
+
+var w = window.innerWidth;
+var h = window.innerHeight; // Canvas width and height
+var players = 0;
+var rings = 0;
+var globalTimer = 100000;
+
+var lx = 0, ly = 0;//center of screen
+var zoom = 1;//proportional to speed
+var zoomMult = 1;
+var wheelRadius = 20;
+
+
+var Img = {}
+loadAllImages();
+
+
+socket.emit('requestBody');
+
+
+function loadImage (name, src) {
+	if (Img[name]) {console.error("Loading image twice: " + name); return;}
+	Img[name] = new Image();
+	Img[name].src = src;
+}
+function loadAllImages(){
+	loadImage("bike", '/img/bike.png');
+}
+
+
+
+function render(){
+	if(empty == 0 || ops > 0)
+		return;
+	ctx.fillStyle = "black";
+	ctx.fillRect(0,0,w,h);
+	frames++;
+	ops++;
+	
+	ctx.save();
+	ctx.translate(w/2, h/2);
+	ctx.scale(zoom*zoomMult, zoom*zoomMult);
+	ctx.translate(-w/2, -h/2);
+	rEdges();
+	rPlayers();
+	rRings();
+	ctx.restore();
+	
+	rMinimap();
+	if(multiplayer)
+		rChat();
+	if(multiplayer)
+		rLB();
+	rTut();
+	rTimer();
+	ops--;
+}
+
+
+//packet handling
+socket.on('posUp', function (data) {
+	players = data.players;
+	empty = 1;
+	globalTimer = data.globalTimer;
+});
+socket.on('chat', function (data) {
+	for (var i = chatLength; i > 0; i--)
+		messages[i] = messages[i - 1];
+	messages[0] = data.msg;
+});
+socket.on('you', function (data) {
+	zoom = (99*zoom + 10/(10+Math.hypot(lx-data.lx,ly-data.ly)))/100;
+	lx=data.lx;
+	ly=data.ly;
+});
+socket.on('reping',function(data){
+	let d = new Date();
+	var time = d.getTime();
+	nLag = time - data.time;
+});
+socket.on('newMap',function(data){
+	rings = data.rings;
+});
+
+setInterval(function(){
+	let d = new Date();
+	var time = d.getTime();
+	socket.emit('pingmsg', {time:time});
+},1000);
+setInterval(function(){
+	w = window.innerWidth;
+	h = window.innerHeight;
+	if(canvas.width != w || canvas.height != h){
+		canvas.width = w;
+		canvas.height = h;
+	}
+},40);
+setInterval(function(){
+	render();
+},20);
+
+
+//input
+document.onkeydown = function (event) {
+//	if (event.keyCode === 16)
+//		shift = true;
+//	else if(typing)
+//		if (event.keyCode == 13){
+//			socket.emit('chat', chatMessage);
+//			typing = false;
+//		}
+//	else {
+		didW = true;
+		if(keyboard[event.keyCode] == true)
+			return;
+		keyboard[event.keyCode] = true;
+		if (event.keyCode == 13)
+			typing = true;
+		else if (event.keyCode === 83 || event.keyCode === 40)//s
+			socket.emit('key', { inputId: 's', state: true });
+		else if (event.keyCode === 87 || event.keyCode === 38)//w
+			socket.emit('key', { inputId: 'w', state: true });
+		else if (event.keyCode === 65 || event.keyCode === 37)//a
+			socket.emit('key', { inputId: 'a', state: true });
+		else if (event.keyCode === 68 || event.keyCode === 39)//d
+			socket.emit('key', { inputId: 'd', state: true });
+		else if (event.keyCode === 72)//h
+			multiplayer = !multiplayer;
+		else if (event.keyCode === 32)//space
+			socket.emit('key', { inputId: ' ', state: true });
+		else
+			didW = false;
+//	}
+}
+document.onkeyup = function (event) {
+	if(keyboard[event.keyCode] == false)
+		return;
+	keyboard[event.keyCode] = false;
+	if (event.keyCode === 83 || event.keyCode === 40)//s
+		socket.emit('key', { inputId: 's', state: false });
+	else if (event.keyCode === 87 || event.keyCode === 38)//w
+		socket.emit('key', { inputId: 'w', state: false });
+	else if (event.keyCode === 65 || event.keyCode === 37)//a
+		socket.emit('key', { inputId: 'a', state: false });
+	else if (event.keyCode === 68 || event.keyCode === 39)//d
+		socket.emit('key', { inputId: 'd', state: false });
+	else if (event.keyCode === 32)//space
+		socket.emit('key', { inputId: ' ', state: false });
+	else if (event.keyCode === 16)
+		shift = false;
+}
+document.addEventListener('mousewheel', function (evt) {
+	zoomMult *= 1+.2*Math.sign(evt.wheelDelta);
+}, false);
+
+
+
+//random
+function write(str, x, y){
+	ctx.fillText(str, x, y);
+}
+function square(x){
+	return x * x;
+}
+function cube(x){
+	return x * x * x;
+}
+function lerp(a,b,w){
+	return a * (1 - w) + b * w;
+}
+function intToTime(x){
+	var minutes=Math.floor(x/50/60), seconds=""+Math.floor(x/50%60), dec=(""+(x%50)*2).substring(0,2);
+	if(seconds.length<2)
+		seconds = "0"+seconds;
+	if(dec.endsWith("."))
+		dec = dec.substring(0,1);
+	while(dec.length < 2)
+		dec = "0"+dec;
+	return minutes+":"+seconds+"."+dec;
+}
+
+
+function rChat(){
+	ctx.textAlign = "left";
+	ctx.font = "11px Telegrama";
+	
+	ctx.fillStyle = "white";
+	for (var i = 0; i < 100; i++){
+		ctx.globalAlpha = (19-i) / 20;
+		write(messages[i],16,h-32-12*i);
+	}
+	ctx.globalAlpha = 1;
+}
+function rLB(){
+	
+	ctx.fillStyle = 'yellow';
+	ctx.font = "24px Telegrama";
+	ctx.textAlign = "center";
+	write("Leaderboard", w - 128, 28);
+	ctx.font = "11px Telegrama";
+	ctx.fillStyle = 'yellow';
+	write("Name", w - 208, 48);
+	ctx.textAlign = "right";
+	write("Time", w - 48 - 16, 48);
+	var lb = [];
+	var editPlayers = {};
+	for(var i in players)
+		editPlayers[i] = players[i];
+	while(Object.keys(editPlayers).length>0){
+		var bestIndex = 0;
+		for(var i in editPlayers){
+			if(bestIndex == 0)
+				bestIndex = i;
+			if(editPlayers[i].timer < editPlayers[bestIndex].timer)
+				bestIndex = i;
+		}
+		lb.push(editPlayers[bestIndex]);
+		delete editPlayers[bestIndex];
+	}
+	for(var i = 0; i < lb.length; i++){
+		ctx.font = "11px Telegrama";
+		var place = 1 + i;
+		ctx.fillStyle = lb[i].finished?"lime":"yellow";
+		ctx.textAlign = "left";
+		write(lb[i].name, w - 216, (i+4)*16);
+		ctx.fillStyle = 'yellow';
+		write(place + ".", w - 248, (i+4)*16);
+		ctx.textAlign = "right";
+		write(intToTime(lb[i].timer), w - 48 - 16, (i+4)*16);
+		if(lb[i].finished && i < 3){
+			ctx.fillStyle = ["gold", "silver","bronze"][i];;
+			ctx.font = "32px Telegrama";
+			write(lb[i].name, w-400, i*32+64);
+		}
+	}
+}
+function rTut(){
+	if(didW)
+		return;
+	ctx.save();
+	ctx.textAlign = "center";
+	ctx.fillStyle = 'cyan';
+	var date = new Date();
+	var ms = date.getTime();
+	ctx.font = (5 * Math.sin(ms / 180) + 35) + "px Telegrama";
+	write("Use WASD to move!", w / 2, 48);
+	write("Use space to pass through walls!", w / 2, 80);
+	write("Go as far right as you can!", w / 2, 112);
+	ctx.restore();
+}
+function rTimer(){
+	if(globalTimer < 48) {
+		ctx.textAlign = "center";
+		ctx.font = "128px Telegrama";
+		ctx.fillStyle=["lime", "yellow","orange","red"][Math.floor((globalTimer-49)/-50)];
+		write(globalTimer<0?(""+(Math.floor(globalTimer/-50)+1)):"GO!",w/2,h-64);
+	}else{
+		ctx.textAlign = "center";
+		ctx.font = "32px Telegrama";
+		ctx.fillStyle="lime";
+		write(intToTime(globalTimer),w/2,h-64);
+	}
+	if(globalTimer > 5750){
+		ctx.textAlign = "center";
+		ctx.font = "32px Telegrama";
+		ctx.fillStyle="red";
+		write(Math.floor((6000-globalTimer)/50+1)+"!",w/2,h-96);
+	}
+}
+function circle(x,y,r){
+	ctx.save();
+	ctx.translate(x,y);
+	ctx.beginPath();
+	ctx.arc(0,0,r,0,2*Math.PI);
+	ctx.stroke();
+	ctx.restore();
+}
+function rMinimap(){
+	ctx.lineWidth = 4;
+	ctx.save();
+	
+	ctx.strokeStyle = "red";
+	ctx.beginPath();
+	ctx.moveTo(16+128,16);
+	ctx.lineTo(16,16);
+	ctx.lineTo(16+128,16+128);
+	ctx.stroke();
+	
+	ctx.strokeStyle = "lime";
+	ctx.beginPath();
+	ctx.moveTo(16+128,16);
+	ctx.lineTo(16+128,16+128);
+	ctx.stroke();
+	
+	for(var i in players){
+		var player = players[i];
+		ctx.fillStyle = player.color;
+		ctx.fillRect(16+player.lx*128/16384-2,16+player.ly*128/16384-2,4,4);
+	}
+	
+	ctx.restore();
+}
+
+
+function rEdges(){
+	ctx.save();
+	ctx.setLineDash([30,30]);
+	ctx.lineWidth = 5*zoom*zoomMult+10;
+	ctx.strokeStyle = "red";
+	ctx.beginPath();
+	ctx.moveTo(16384 - lx + w / 2, 0 - ly + h / 2);
+	ctx.lineTo(0 - lx + w / 2, 0 - ly + h / 2);
+	ctx.lineTo(16384 - lx + w / 2, 16384 - ly + h / 2);
+	ctx.stroke();
+	
+	ctx.strokeStyle = "green";
+	ctx.lineWidth = 50*zoom*zoomMult+30;
+	ctx.beginPath();
+	ctx.moveTo(16384 - lx + w / 2, 0 - ly + h / 2);
+	ctx.lineTo(16384 - lx + w / 2, 16384 - ly + h / 2);
+	ctx.stroke();
+	ctx.restore();
+}
+function rRings(){
+	ctx.lineWidth = 5*zoom*zoomMult+4;
+	for (var i in rings) {
+		var selfo = rings[i];
+		var rendX = selfo.x - lx + w / 2;
+		var rendY = selfo.y - ly + h / 2;
+		ctx.fillStyle = ctx.strokeStyle = selfo.color;
+
+		circle(rendX, rendY, selfo.r);
+	}
+}
+function rPlayers(){
+	var t = globalTimer / 10.;
+	var flasher = ((Math.floor(Math.cos(t) * 128 + 128) << 16) + (Math.floor(Math.cos(t+Math.PI*2/3) * 128 + 128) << 8) + Math.floor(Math.cos(t+Math.PI * 4 / 3) * 128 + 128)).toString(16)
+	if(flasher.length < 6)
+		flasher = "0"+flasher;
+	flasher = "#"+flasher;
+	ctx.textAlign = "center";
+	for (var i in players) {
+		var selfo = players[i];
+		var img = Img.bike;
+		var rendX = selfo.lx - lx + w / 2;
+		var rendY = selfo.ly - ly + h / 2;
+		
+		ctx.font = "40px Telegrama";
+		ctx.globalAlpha = selfo.space?.5:1;
+		ctx.save();
+		ctx.translate(rendX,rendY);
+		ctx.rotate(selfo.la);
+		ctx.fillStyle = selfo.trail?flasher:selfo.color;
+		if(multiplayer)
+			write(selfo.name, 0, -100);
+		ctx.drawImage(img, -img.width / 4, -img.height / 2, img.width/2, img.height/2);
+		ctx.restore();
+
+		
+		
+		ctx.strokeStyle = selfo.color;
+		ctx.lineWidth = 10*zoom*zoomMult;
+		circle(selfo.w1x - lx + w / 2, selfo.w1y - ly + h / 2, 20);
+		circle(selfo.w2x - lx + w / 2, selfo.w2y - ly + h / 2, 20);
+	}
+	ctx.strokeStyle = "black";
+	ctx.globalAlpha = 1;
+	ctx.textAlign = "left";
+}
+
